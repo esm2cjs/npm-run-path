e8008f4f2f815678823ad5f4df0a3a3e24f83f91
AlCalzone <d.griesel@gmx.net>
2022-08-18 23:29:44 +0200

    fix: typo in xo ignores

--- .github/make_hybrid.sh
+++ .github/make_hybrid.sh
@@ -31,7 +31,7 @@ PJSON=$(cat package.json | jq --tab '
 	| .typesVersions["*"]["*"] = ["esm/*"]
 	| .scripts["to-cjs"] = "esm2cjs --in esm --out cjs -t node12"
 	| .dependencies["path-key"] |= "npm:@esm2cjs/path-key@" + .
-	| .xo = {ignores: ["cjs", "*.test-d.ts"]}
+	| .xo = {ignores: ["cjs", "**/*.test-d.ts"]}
 ')
 echo "$PJSON" > package.json
 
